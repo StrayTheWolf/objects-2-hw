518ca035101b45579e7bd34b0c82fd8a0152f52f
Wolf Stanislav <ork2004@yandex.ru>
2021-03-30 21:52:37 +0300

    added class Post2

--- scripts/script.js
+++ scripts/script.js
@@ -173,3 +173,56 @@ post1.dateNow = '2021 01 01';
 console.log(post1.isAllow);
 console.log(Object.keys(post1));
 console.log(post1)
+
+
+console.log('Класс POST')
+
+
+class Post2 {
+
+    constructor(title, text, published_at, published, deleted) {
+
+        Object.defineProperties(this, {
+
+            'text': {
+                value: text,
+                enumerable: true,
+                writable: true,
+                configurable: false
+            },
+
+            'title': {
+                value: title,
+                enumerable: true,
+                writable: true,
+                configurable: false
+            },
+        })
+
+        this.published = published;
+        this.deleted = deleted;
+    }
+
+
+    get isAllow() {
+        return this.published === true && this.deleted === true;
+    }
+
+    get published_at() {
+
+        let date = new Date();
+        let fullYear = date.getFullYear()
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        return fullYear + ', ' + hours + ':' + minutes;
+    }
+}
+
+let post2 = new Post2('Заголовок статьи Класс', 'Содержимое статьи Класс', Date.now(), true, false)
+
+console.log(post2.title);
+console.log(post2.text);
+console.log(post2.published_at);
+console.log(post2.isAllow);
+console.log(Object.keys(post2));
+console.log(post2)
